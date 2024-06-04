c21302b5c045e84beaabda39302c7eddc4f37a6b
mirnaiem <mirnaiem8teen@gmail.com>
2024-06-04 17:07:04 +0600

    remove port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors=require('cors')
 const dotenv = require('dotenv')
 dotenv.config();
 const app = express()
-const port = process.env.PORT || 3000;
+const port =  3000;
 app.use(cors())
 app.use(express.json())
 
